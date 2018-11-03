79b9db738b8a3763288fc9d477c043e50dc97a15
Lanpei Li <lilanpei@gmail.com>
2018-11-03 16:06:05 +0100

    Modify function gen_batch to make sure that there are samples of each class in every batch

--- ADNI.py
+++ ADNI.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import numpy as np
 import nibabel as nib
@@ -23,7 +17,7 @@ n_folds = 5
 batch_size = 5
 n_epochs = 150
 save_dir = os.path.join(os.getcwd(), 'saved_models')
-ds_path = '/home/lilanpei/work/ADNI'
+ds_path = 'H:/Jupyter/ISPR_ADNI/input'
 historyloglocation_train  = './{}_traininghistory_train.txt'.format(str(time.time()))
 historyloglocation_test  = './{}_traininghistory_train.txt'.format(str(time.time()))
 model_name = 'ADNI_VoxCNN'
@@ -118,13 +112,12 @@ def gen_folds(data_1,data_2,label):
     indices_folds = list(range(n_folds))
     folds_1 = list(StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=1).split(data_1,label[:len(data_1),0]))
     folds_2 = list(StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=1).split(data_2,label[len(data_1):len(data_1)+len(data_2),0]))
-    while True:
-        for i in indices_folds:
-            x_train = np.concatenate((data_1[folds_1[i][0]],data_2[folds_2[i][0]]),axis=0)
-            y_train = np.concatenate((label[folds_1[i][0]],label[folds_2[i][0]+len(data_1)]),axis=0)
-            x_test = np.concatenate((data_1[folds_1[i][1]],data_2[folds_2[i][1]]),axis=0)
-            y_test = np.concatenate((label[folds_1[i][1]],label[folds_2[i][1]+len(data_1)]),axis=0)
-            yield x_train,y_train,x_test,y_test
+    for i in indices_folds:
+        x_train = np.concatenate((data_1[folds_1[i][0]],data_2[folds_2[i][0]]),axis=0)
+        y_train = np.concatenate((label[folds_1[i][0]],label[folds_2[i][0]+len(data_1)]),axis=0)
+        x_test = np.concatenate((data_1[folds_1[i][1]],data_2[folds_2[i][1]]),axis=0)
+        y_test = np.concatenate((label[folds_1[i][1]],label[folds_2[i][1]+len(data_1)]),axis=0)
+        yield x_train,y_train,x_test,y_test
 
 def gen_batch(data,label):
     print ("in batch generator")
@@ -132,28 +125,99 @@ def gen_batch(data,label):
     len_1 = list(counter.items())[0][1]
     len_2 = list(counter.items())[1][1]
     #print(counter.elements)
+    #print(list(counter.items())[0][0],list(counter.items())[0][1],list(counter.items())[1][0],list(counter.items())[1][1])
+    #print(len_1,len_2)
     data_1_gen = gen_data(data[:len_1,],label[:len_1,])
     data_2_gen = gen_data(data[len_1:len_1+len_2,],label[len_1:len_1+len_2,])
-    #while True:
-    x_batch = []
-    y_batch = []
-    x_1, y_1 = next(data_1_gen)
-    x_batch.append(x_1)
-    y_batch.append(y_1)
-    x_2, y_2 = next(data_2_gen)
-    x_batch.append(x_2)
-    y_batch.append(y_2)
-    for _ in range(batch_size-len(x_batch)):
-        if random.randint(1,100) <= 50:
-            x_3, y_3 = next(data_1_gen)
-            x_batch.append(x_3)
-            y_batch.append(y_3)
-        else:
-            x_4, y_4 = next(data_2_gen)
-            x_batch.append(x_4)
-            y_batch.append(y_4)
-                
-    yield np.array(x_batch),np.asarray(y_batch)
+    ds_1_cnt = 0
+    ds_2_cnt = 0
+    flag = 0
+    while True:
+        x_batch = []
+        y_batch = []
+        batch_cnt = 0
+        if len_1 >= len_2:
+            while batch_cnt+2 < batch_size:
+                if ds_1_cnt < len_1:
+                    x_1, y_1 = next(data_1_gen)
+                    x_batch.append(x_1)
+                    y_batch.append(y_1)
+                    ds_1_cnt += 1
+                    batch_cnt += 1
+                    x_2, y_2 = next(data_2_gen)
+                    x_batch.append(x_2)
+                    y_batch.append(y_2)
+                    ds_2_cnt += 1
+                    batch_cnt += 1
+                else:
+                    break
+            #print("<<<",ds_1_cnt,len_1)
+            if ds_1_cnt < len_1:
+                #print("<<<",batch_cnt,batch_size)
+                if batch_cnt < batch_size:
+                    if flag == 0:
+                        x_2, y_2 = next(data_2_gen)
+                        x_batch.append(x_2)
+                        y_batch.append(y_2)
+                        ds_2_cnt += 1
+                        batch_cnt += 1
+                        flag = 1
+                    else:
+                        x_1, y_1 = next(data_1_gen)
+                        x_batch.append(x_1)
+                        y_batch.append(y_1)
+                        ds_1_cnt += 1
+                        batch_cnt += 1
+                        flag = 0
+                batch_cnt = 0
+            else:
+                #print("$$$",ds_1_cnt,ds_2_cnt)
+                if batch_cnt != 0:
+                    batch_cnt = 0
+                    yield np.array(x_batch),np.asarray(y_batch)
+                break
+        else: 
+            while batch_cnt+2 < batch_size:
+                if ds_2_cnt < len_2:
+                    x_2, y_2 = next(data_2_gen)
+                    x_batch.append(x_2)
+                    y_batch.append(y_2)
+                    ds_2_cnt += 1
+                    batch_cnt += 1
+                    x_1, y_1 = next(data_1_gen)
+                    x_batch.append(x_1)
+                    y_batch.append(y_1)
+                    ds_1_cnt += 1
+                    batch_cnt += 1
+                else:
+                    break
+            #print("<<<",ds_2_cnt,len_2)
+            if ds_2_cnt < len_2:
+                if batch_cnt < batch_size:
+                    if flag == 0:
+                        x_1, y_1 = next(data_1_gen)
+                        x_batch.append(x_1)
+                        y_batch.append(y_1)
+                        ds_1_cnt += 1
+                        batch_cnt += 1
+                        flag = 1
+                    else:
+                        x_2, y_2 = next(data_2_gen)
+                        x_batch.append(x_2)
+                        y_batch.append(y_2)
+                        ds_2_cnt += 1
+                        batch_cnt += 1
+                        flag = 0
+                    batch_cnt = 0
+            else:
+                #print("$$$",ds_1_cnt,ds_2_cnt)
+                if batch_cnt != 0:
+                    batch_cnt = 0
+                    yield np.array(x_batch),np.asarray(y_batch)
+                break
+
+        #print("###",ds_1_cnt,ds_2_cnt)
+        yield np.array(x_batch),np.asarray(y_batch)
 
 def one_vs_one_train(ds_name_1,ds_name_2):
     ds_1, ds_2, lb = get_data_set(ds_name_1,ds_name_2)
@@ -163,10 +227,11 @@ def one_vs_one_train(ds_name_1,ds_name_2):
     model.compile(loss='categorical_crossentropy',optimizer=opt, metrics=['accuracy'])
     for x_train,y_train,x_test,y_test in folds_gen:
         batch_gen = gen_batch(x_train,y_train)
+        print("Total number of batch in fold",sum(1 for x in batch_gen))
         for i in range(n_epochs):
             for x_batch, y_batch in batch_gen:
                 history_train = model.fit(x_batch, y_batch, epochs=1)
-                print ('epoch: ', i)
+                print ('######epoch: ', i)
                 print ('loss: ',history_train.history['loss'][0])
                 with open(historyloglocation_train,"a+") as f:
                     f.write('{},{},{}\n'.format(str(i), str(history_train.history['loss'][0]), str(history_train.history['acc'][0])))
@@ -177,29 +242,6 @@ def one_vs_one_train(ds_name_1,ds_name_2):
                 f.write('{},{},{}\n'.format(str(i), str(history_eval.history['loss'][0]), str(history_train.history['acc'][0])))
             #model.save_weights(save_dir)
             #model.save(save_dir)    
+
 for i in range(len(ds_name)):
     one_vs_one_train(str(ds_name[i][0]),str(ds_name[i][1]))
-
-
-# In[11]:
-
-
-import matplotlib.pyplot as plt
-print(history.history.keys())
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-#plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-#plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
