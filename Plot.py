9f5bbadf3003a6adba5c00b6963c99c51e37cb55
Lanpei Li <lilanpei@gmail.com>
2018-11-06 22:11:51 +0100

    modify function get_plot_data : only load corresponding data

--- Plot.py
+++ Plot.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,21 +5,21 @@ ds_name = [["AD","NC"],["AD","EMCI"],["AD","LMCI"],["LMCI","NC"],["LMCI","EMCI"]
 plot_history_path="H:\Google Drive\plot"
 plot_data_type= ["ROC_AUC", "evaluation", "training"]
 
-def get_plot_data(history_path,prefix):
+def get_plot_data(history_path, prefix, plot_data_type):
+
     whole_history_list = list()
     whole_data_list = list()
-    for i in range(len(ds_name)):
-        history_list = list()
-        data_list = list()
-        history_txt_prefix="{}\\{}_history_{}_vs_{}".format(str(history_path),str(prefix),str(ds_name[i][0]),str(ds_name[i][1]))
-        for j in range(1,len(ds_name)):
-            history_txt="{}_fold_index_{}.txt".format(str(history_txt_prefix),str(j))
-            history_list.append(history_txt)
-            #print(history_txt)
-            data = np.loadtxt(history_txt, delimiter=',', usecols = 1)
-            data_list.append(data)
-        whole_history_list.append(history_list)
-        whole_data_list.append(data_list)
+    history_list = list()
+    data_list = list()
+    history_txt_prefix="{}\\{}_history_{}_vs_{}".format(str(history_path),str(plot_data_type),str(prefix[0]),str(prefix[1]))
+    for j in range(1,len(ds_name)):
+        history_txt="{}_fold_index_{}.txt".format(str(history_txt_prefix),str(j))
+        history_list.append(history_txt)
+        #print(history_txt)
+        data = np.loadtxt(history_txt, delimiter=',', usecols = 1)
+        data_list.append(data)
+    whole_history_list.append(history_list)
+    whole_data_list.append(data_list)
     return np.asanyarray(whole_data_list)
 
 def plot_ROC_AUC(ROC_AUC,prefix):
@@ -42,5 +41,5 @@ def plot_ROC_AUC(ROC_AUC,prefix):
     plt.legend(['Validation ROC AUC mean', 'Validation ROC AUC std'], loc='lower left')
     plt.show()
        
-ROC_AUC = get_plot_data(plot_history_path,plot_data_type[0])
+ROC_AUC = get_plot_data(plot_history_path, ds_name[0], plot_data_type[0])
 plot_ROC_AUC(ROC_AUC,ds_name[0])
